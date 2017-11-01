51f996c897d0dbd18b8d06f46b708f5b93bf23a3
remomaster <remomaster4@hotmail.com>
2017-11-01 12:15:31 +0100

    Add files via upload

--- algo.py
+++ algo.py
@@ -1,18 +1,25 @@
 import random as rand
 
+#Algorithmus wurde extra ((x,y),z) geklammert, damit keine durch null Rechnung entsteht
+def SollFunktion(x):
+	return (x[0] / x[1]) - x[2]		
+
 #testen der lösung
-def tester(input,anleitung1,anleitung2):
+def generierterAgorithusTesten(input,anleitung1,anleitung2):
 	for x in input:
-		result = (x[0] + x[1]) / x[2]							#  'naive' lösung zum testen
-		berechnet = anleitung2(anleitung1(x[0],x[1]),x[2]) 		#  'algo'  output berechnen
-		if result != berechnet:
+		berechnetResultat = anleitung2(anleitung1(x[0],x[1]),x[2]) 		#  output von generiertem Algorithmus berechnen
+		if SollFunktion(x) != berechnetResultat:						#  prüfen ob berechnetes Resultat korrekt ist.
 			return False
 	return True
 		
 		
 # algo repetoir festlegen
-repetoirname = ['add','sub','mul','div']
-repetoir1 = [lambda x,y : x+y,lambda x,y : x-y,lambda x,y : x*y,lambda x,y : x/y if y != 0 else -1]
+repetoir1 = {
+			 'add' : lambda x,y : x+y, \
+			 'sub' : lambda x,y : x-y, \
+			 'mul' : lambda x, y : x*y, \
+			 'div' : lambda x,y : x/y if y != 0 else -1
+			}
 
 algo = []
 inputs = [[rand.randint(1,10),rand.randint(1,10),rand.randint(1,10)] for x in range(10)]				#test input generieren
@@ -20,8 +27,8 @@ inputs = [[rand.randint(1,10),rand.randint(1,10),rand.randint(1,10)] for x in ra
 
 #algorithmus finden
 while algo == []:
-	next = [repetoir1[rand.randint(0,len(repetoir1)-1)],repetoir1[rand.randint(0,len(repetoir1)-1)]]	#algorithmus generieren
-	if tester(inputs,next[0],next[1]):																	#algorithmus testen
-		algo = repetoirname[repetoir1.index(next[0])],repetoirname[repetoir1.index(next[1])]
+	next = [list(repetoir1.keys())[rand.randint(0,len(repetoir1)-1)], list(repetoir1.keys())[rand.randint(0,len(repetoir1)-1)]]		#algorithmus generieren
+	if generierterAgorithusTesten(inputs,repetoir1[next[0]],repetoir1[next[1]]):													#algorithmus testen
+		algo = next
 
 print(' '.join(algo))
